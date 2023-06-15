1e396e3bd6bea6db2b5a189d65894f00ed8c28cb
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-15 07:28:46 -0700

    writing a C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -- Entry point
+ * This is to print "Programming is like building a multilingual puzzle"
+ * Return 0, to tell the Processor it was a success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");	      return (0);
+}
