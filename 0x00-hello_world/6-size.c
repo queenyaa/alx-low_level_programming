bbe7cfb1d8602e06d327b4125d84d68c92515cec
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-15 09:30:55 -0700

    sizes of variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,9 @@
  * Return: Always 0 (Success)
  */
 
-/*the main function is the entry point*/
 int main(void)
 {
+	/*the main function is the entry point*/
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
