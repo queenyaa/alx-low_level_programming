c11d738689acf35eefab1bd9e4000ebdedede581
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-15 07:49:20 -0700

    Creating a C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  * Return: 0, success
  */
 
-int main(void)
+int main()
 {
-	puts("\"Programming is like building a multilingual puzzle");	      return (0);
+	puts("Programming is like building a multilingual puzzle");	      return (0);
 }
