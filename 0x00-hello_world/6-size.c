1825d3ac53e3eefabb6d9f7a37220df6fe725c45
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-15 09:55:30 -0700

    generating the intel syntax assembly code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@
  */
 
 int main(void)
+/*the main function is the entry point*/
 {
-	/*the main function is the entry point*/
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
