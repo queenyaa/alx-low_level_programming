507d1c0f12c6020e6f057d5fb28c4986c2b59e7a
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-16 11:35:46 -0700

    three digit combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,20 +13,17 @@ int main(void)
 
 	for (a = 0; a < 100; a++)
 	{
-		for (b = 0; j < 100; j++)
+		for (b = a; j < 100; j++)
 		{
-			if (a < b)
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a != 99 || b != 99)
 			{
-				putchar((a / 10) + 48);
-				putchar((a % 10) + 48);
+				putchar(',');
 				putchar(' ');
-				putchar((b / 10) + 48);
-				putchar((b % 10) + 48);
-				if (a != 98 || b != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
 		}
 	}
