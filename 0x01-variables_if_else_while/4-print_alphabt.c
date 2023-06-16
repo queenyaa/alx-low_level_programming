fc91017d41f1532f8adc60d12df7d1bb73ea29d3
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-16 09:48:37 -0700

    we don't want to see q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: program to print lowercase alphabet without q and e
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char v = 'a';
+
+	while (v <= 'z')
+	{
+		if (v != 'q' && v != 'e')
+			putchar(v);
+		v++;
+	}
+	putchar('\n\);
+
+	return (0);
+}
