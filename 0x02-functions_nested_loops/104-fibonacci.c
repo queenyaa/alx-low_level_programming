3076623f4c5b33fe13876734b3ef85e6ef0fa57f
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-20 15:54:46 -0700

    some 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 	unsigned long int a;
 	unsigned long int b = 1;
 	unsigned long int c = 2;
-	unsigned long int 1 = 1000000000;
+	unsigned long int d = 1000000000;
 	unsigned long int b1;
 	unsigned long int b2;
 	unsigned long int c1;
@@ -24,15 +24,15 @@ int main(void)
 		c += b;
 		b = c - b;
 	}
-	b1 = (b / 1);
-	b2 = (b % 1);
-	c1 = (c / 1);
-	c2 = (c % 1);
+	b1 = (b / d);
+	b2 = (b % d);
+	c1 = (c / d);
+	c2 = (c % d);
 
 	for (a = 92; a < 99; ++a)
 	{
-		printf(", %lu", c1 + (c2 / 1));
-		printf("%lu", c2 % 1);
+		printf(", %lu", c1 + (c2 / d));
+		printf("%lu", c2 % d);
 		c1 = c1 + b1;
 		b1 = c1 - b1;
 		c2 = c2 + b2;
