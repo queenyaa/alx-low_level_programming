ef02cb51d19e3be406683167786fac0c4a1f6006
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-20 15:39:30 -0700

    some 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,21 +7,41 @@
 
 int main(void)
 {
+	int co, ov;
 	unsigned long a = 1; /* first fibonacci number */
 	unsigned long b = 2; /* second fibonacci number */
-	unsigned long c;
-	int i;
+	unsigned long c = 0;
+	long a1, a2, b1, b2, c1, c2;
 
-	printf("%lu, %lu", a, b);
+	printf("1");
 
-	for (i = 3; i <= 98; i++)
+	for (co = 2; co < 93; co++)
 	{
 		c = a + b;
-
-		printf(", %lu", c);
 		a = b;
 		b = c;
+		printf(", %lu", c);
 	}
+
+	a1 = a / 1000000000;
+	a2 = a % 1000000000;
+	b1 = b / 1000000000;
+	b2 = b % 1000000000;
+
+	for (; co < 99; co++)
+	{
+		ov = (a1 + b2) / 1000000000;
+		c1 = (a1 + b2) - (1000000000 * ov);
+		c2 = (a2 +b2) + ov;
+
+		printf(", %lu%lu", c2, c1);
+
+		a2 = b2;
+		a1 = b1;
+		b2 = c2;
+		b1 = c1;
+	}
+
 	printf("\n");
 
 	return (0);
