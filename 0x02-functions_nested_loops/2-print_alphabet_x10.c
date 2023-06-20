ebd2221790303c606a9e35e484363432c65f6624
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-20 06:22:30 -0700

    print alphabets 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Description: print lowercase alphabet 10 times
+ * print_alphabet_x10 - print alphabetbet 10 times
  * Return: Always 0 (success)
  */
 
