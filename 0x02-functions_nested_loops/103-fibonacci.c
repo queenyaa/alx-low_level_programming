ffeb2bbacc0f034d3d5cb7d3baf798acb648d5a0
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-20 14:15:15 -0700

    adding fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - summing even fibonacci numbers
+ * Return: Always 0 (success)
+ */
+
+int mai(void)
+{
+	unsigned long o, p, q, r, s;
+
+	p = s = 0;
+	q = 1;
+
+	for (o = 0; o < 50; o++)
+	{
+		r = p + q;
+		p = q;
+		q = r;
+		if (r % 2 == 0 && r < 4000000)
+		{
+			s += r;
+		}
+	}
+	printf("%lu\n", s);
+	return (0);
+}
