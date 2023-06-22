5524044172b2218fe7eba90a7202f9ef84400448
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-22 10:29:50 -0700

    drawing triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_tirangle - printing a triangle with #
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int v, w, x;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (v = 1; v <= size; v++)
+	{
+		for (w = 1; w <= (size - v); w++)
+			_putchar(' ');
+
+		for (x = 1; x <= v; x++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
