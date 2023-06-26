c859ea63b369d51845ff724b5c3806f480363339
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-26 16:36:29 -0700

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,31 +8,21 @@
 
 int _atoi(char *s)
 {
+
 	int d = 1;
 	int e = 0;
 	int f = 0;
 
-	if (s[0] == '-') /*checking for sign*/
+	if (s[f] == '-' || s[f] == '+')
 	{
-		d = -1;
+		d = (s[f] == '-') ? -1 : 1;
 		f++;
 	}
-	else if (s[0] == '+')
+
+	while (s[f] >= '0' && s[f] <= '9')
 	{
+		e = e * 10 + (s[f] - '0');
 		f++;
-	}
-	/*converting digit to integer*/
-	while (s[f] != '\0')
-	{
-		if (s[f] >= '0' && s[f] <= '9')
-		{
-			e = e * 10 + (s[f] - '0');
-			f++;
-		}
-		else
-		{
-			break; /*stop at the first non-digit character*/
-		}
 	}
 
 	return (d * e);
