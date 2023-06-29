cd8737c977d95240e7dcc925be699b3dd3301bed
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-29 07:42:04 -0700

    encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encods a string into 1337
+ * @b: input value
+ * Return: b
+ */
+
+char *leet(char *b)
+{
+	int c, d;
+	char *e = "aAeEoOtTlL";
+	char *f = "43071";
+
+	for (c = 0; b[c] != '\0'; c++)
+	{
+		for (d = 0; e[d] != '\0'; d++)
+		{
+			if (b[c] == e[d])
+			{
+				b[c] = f[d];
+				break;
+			}
+		}
+	}
+
+	return (b);
+}
