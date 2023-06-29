a70b4931eb40d4b11fd8bf6c3a6f9004ff8c6b6d
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-06-29 16:45:12 -0700

    print a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,6 +2,59 @@
 #include <stdio.h>
 
 /**
+ * _print_AS - determins n as a printable ASCII char
+ * @n: integer
+ * Return: 1 on success, 0 if not
+ */
+int _print_AS(int n)
+{
+	return (n >= 32 && n <= 126);
+}
+
+/**
+ * print_hex - hex values for string b
+ * @b: string
+ * @i: start position
+ * @j: end
+ */
+void print_hex(char *b, int i, int j)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		if (a < j)
+			printf("%02x", *(b + i + a));
+		else
+			printf("  ");
+		if (a % 2)
+			printf(" ");
+		a++;
+	}
+}
+
+/**
+ * print_ASC - print ascii values for string b,
+ * formatted to replace nonprintable chars with '.'
+ * @b: string
+ * @i: start
+ * @j: end
+ */
+void print_ASC(char *b, int i, int j)
+{
+	int d, e = 0;
+
+	while (e < j)
+	{
+		d = *(b + e + i);
+		if (!_print_AS(d))
+			d = 46;
+		printf("%c", (char)d);
+		e++;
+	}
+}
+
+/**
  * print_buffer -prints buffer
  * @b: buffer
  * @size: size
@@ -14,32 +67,15 @@ void print_buffer(char *b, int size)
 
 	if (size <= 0)
 	{
-		printf("\n");
-		return;
+		for (i = 0; i < size; i += 10)
+		{
+			j = (size - i < 10) ? size - i : 10;
+			printf("%08x: ", i);
+			print_hex(b, i, j);
+			print_ASC(b, i, j);
+			printf("\n");
+		}
 	}
-	for (i = 0; i < size; i += 10)
-	{
-		printf("$08x: ", i);
-
-		for (j = i; j < i + 10; j++ && j < size; j++)
-		{
-			printf("%02x%s", (unsigned char)b[j])
-			if (j % 2 != 0)
-				printf(" ");
-		}
-		for (; j < i + 10; j++)
-		{
-			printf("  ");
-			if (j % 2 != 0)
-				printf(" ");
-		}
-		for (j = i; j < i + 10 && j < size; j++)
-		{
-			if (b[j] >= 32 && b[j] <= 126);
-				printf("%c", b[j]);
-			else
-				printf(".");
-		}
+	else
 		printf("\n");
-	}
 }
