5a6acd36aa2b99d29cf114d517e61bb55ee10ccf
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-07-03 06:20:54 -0700

    locates a character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,12 +13,11 @@ char *_strchr(char *s, char c)
 	int a = 0;
 
 	while (s[a] != '\0' && s[a] != c)
-	{
-		if (s[a] == c)
-		{
-			return (&s[a]);
-		}
 		a++;
-	}
-	return (NULL);
+
+	if (s[a] == c)
+		return (&s[a]);
+
+	else
+		return (NULL);
 }
