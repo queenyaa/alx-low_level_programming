d10d0dedca2ea41d6806c43ed5be5769d0472635
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-07-03 08:04:14 -0700

    prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,6 +3,7 @@
 /**
  * print_chessboard - print chessboard given 2D array
  * @a: 2D array
+ * Return: Always 0 (success)
  */
 
 void print_chessboard(char (*a)[8])
@@ -12,10 +13,7 @@ void print_chessboard(char (*a)[8])
 	for (x = 0; x < 8; x++)
 	{
 		for (y = 0; y < 8; y++)
-		{
 			_putchar(a[x][y]);
-			_putchar(' ');
-		}
 		_putchar('\n');
 	}
 }
