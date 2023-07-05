2608722f7902b7f16350ad604c3562f200bdfe8a
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-07-05 10:49:27 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,39 +1,52 @@
 #include "main.h"
 
 /**
- * pal_help - check start and end characters for equality
- * @st: start index
- * @end: end index
+ * is_palindrome - check string if it's a palindrome
  * @s: string
- * Return: 1 palindrome , 0 otherwise
- */
-
-int pal_help(char *s, int st, int end)
-{
-	if (st >= end)
-	{
-		return (1); /*base case: reached mid, string is palindrome*/
-	}
-	if (s[st] != s[end])
-	{
-		return (0); /*char st and end don't match*/
-	}
-	return (pal_help(s, st + 1, end - 1)); /*recurse next st and end*/
-}
-
-/**
- * is_palindrome - check if a string is a palindrome
- * @s: char array string
- * Return: 1 if palindrome, 0 if not
+ * Return: 1 if palindrom, 0 if not
  */
 
 int is_palindrome(char *s)
 {
-	int length = 0;
+	int length(char *s);
+	int pal_help(char *s, int l);
+	int ln; /*length*/
 
-	while (s[length] != '\0')
+	ln = length(s) - 1;
+
+	return (pal_help(s, --ln));
+}
+
+/**
+ * length - gets length of string
+ * @s: string
+ * Return: length of s
+ */
+
+int length(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	else
+		return (1 + length(++s));
+}
+
+/**
+ * pal_help - check start and end characters for equality
+ * @l: length
+ * @s: string
+ * Return: 1 palindrome , 0 otherwise
+ */
+
+int pal_help(char *s, int l)
+{
+	if (*s == *(s + l))
 	{
-		length++; /*Calculate the length of the string*/
+		if (l <= 0)
+			return (1);
+		else
+			return (pal_help(++s, l - 2));
 	}
-	return (pal_help(s, 0, length - 1)); /*call help with indices*/
+	else
+		return (0);
 }
