95a11c419abd78948159a0e5a2e9a75b14071b46
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-07-05 15:20:44 -0700

    comparing string

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+ * st_match - check string after * matches s1
+ * @s1: 1st string
+ * @s2: 2nd string
+ * @aft_wildcd: holder in position right after *
+ * Return: 1 on success, 0 if not
+ */
+
+int st_match(char *s1, char *s2, char *aft_wildcd)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *s2 == '*')
+		return (st_match(s1, s2 + 1, s2 + 1));
+	if (*s1 == '\0' && *s2 != '\0')
+		return (0);
+	if (*s2 == '*')
+		return (st_match(s1, s2 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (st_match(s1 + 1, s2 + 1, aft_wildcd));
+	else
+		return (st_match(s1 + 1, aft_wildcd, aft_wildcd));
+}
+
+/**
  * wildcmp - compares two strings
  * @s1: 1st string
  * @s2: 2nd string
@@ -12,22 +36,10 @@ int wildcmp(char *s1, char *s2)
 	/*when both strings are empty, consider identical*/
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-
-	/*current char match or s2 has '*', cont*/
-	if (*s1 == *s2 || *s2 == '*')
-	{
-		/*s2 had '*', replace any string*/
-		if (*s2 == '*' && *(s2 + 1) == '\0')
-			return (1); /*s2 is '*', replace any string*/
-
-		/*recursive call, compare s1, s2*/
-		if (*s2 == '*' && wildcmp(s1, s2 + 1))
-			return (1); /*s2 is '*', replace any string, skip current char of s1*/
-
-		/*compare next char*/
-		if (*s1 != '\0' && wildcmp(s1 + 1, s2 + ((*s2 == '8') ? 0 : 1)))
-			return (1); /*s1 and s2 match, next char*/
-	}
-	/*if no condition match, strings are not identical*/
-	return (0);
+	if (*s2 == *s1)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s2 == '*')
+		return (st_match(s1, (s2 + 1), (s2 + 1)));
+	else
+		return (0);
 }
