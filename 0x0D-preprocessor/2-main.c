cf84d6b4c75991ab16b4e0d24a31cc594d63f679
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-07-17 05:17:33 -0700

    print my name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Prints name of the current file to stdout
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
