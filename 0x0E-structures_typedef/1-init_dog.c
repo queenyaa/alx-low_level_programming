fceaaa39d9dae0e0becf3ac6f6b3093d03be77d8
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-07-17 07:09:05 -0700

    initializing a variable type

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,10 +11,10 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	/*check if pointer to dog struct is not NULL*/
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
