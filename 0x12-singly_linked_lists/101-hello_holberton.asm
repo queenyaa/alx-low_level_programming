af79d7c81679072b3805a44d5d07a6ff831cd750
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-07-27 06:51:38 -0700

    asm, yaayyy \o/!

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,16 @@
 section .data
-	hello db 'Hello, Holberton', 0
-	format db '%s', 10, 0
+	hello: db 'Hello, Holberton', 0
+	format: db '%s', 10, 0
 
 section .text
 	extern printf, _exit
 
 global main
+
 main:
 	mov rdi, format
 	mov rsi, hello
-	xor rax, rax  ; Chlear RAX to indicate we using varargs
+	mov rax, 0  ; Clear RAX to indicate we using varargs
 	call printf
 
 	; Exit the program
