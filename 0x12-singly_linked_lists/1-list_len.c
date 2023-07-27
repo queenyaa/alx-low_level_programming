81482e7c8ad3b8665155aaa8bcf62796b4b168b4
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-07-27 05:24:19 -0700

    number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements of list_t linked
+ * @h: pointer to head of list
+ *
+ * Return: number of nodes in list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t coconut = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		coconut++;
+	}
+
+	return coconut;
+}
