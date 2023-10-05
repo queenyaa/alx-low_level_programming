22c198725dc3fedca56d5c809d4192fe8bac32b4
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-10-05 07:56:42 -0700

    djb2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Implements the djb2 hash algorith
+ * @str: the string to hash
+ *
+ * Return: the Computed hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+
+	return (hash);
+}
