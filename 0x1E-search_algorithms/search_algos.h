8f354508dd34479cd33b95b9b674968b42264884
Yaa Serwaa Gyan <ysgyan@gmail.com>
2024-01-15 05:46:02 -0800

    10

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,8 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int bin_search(int *array, size_t low, size_t high, int value);
+void print_subarray(int *array, size_t low, size_t high);
 
 #endif /* SEARCH_ALGOS_H */
